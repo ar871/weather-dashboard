2c5ce1340af97a8f6b76e89e679e69bd11b8d8cf
ar871 <arehman871@hotmail.com>
2023-02-03 22:34:26 +0000

    pseudo code added and slight change in html

---
+++ starter/script.js
@@ -0,0 +1,14 @@
+// my api key 3a95d1ed9bf689469735b199ebeae609
+//https://openweathermap.org/api/geocoding-api this will change long and lat to city name
+//**Hint**: Using the 5 Day Weather Forecast API, you'll notice that you will need to pass
+// in coordinates instead of just a city name. Using the OpenWeatherMap APIs, how could we 
+//retrieve geographical coordinates given a city name? 
+// https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys
+
+
+
+//whem user types in city name request is sent to geocoder to find city and return 
+// long and lat which will be used for the weather 5 day weather app to find the weather for the next 5 days in that city
+
+//search will be saved on local stoarage and a "delete histomey button" will be available if user wants to delete history.
+// // will porpbly have to use moment js to tell what time it is and what the next couple days are gonna be.
